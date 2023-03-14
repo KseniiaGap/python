f2a157d8346706d7287bcdf7aed01257530a8c53
Kseniia <kseniyaga@gmail.com>
2023-03-14 17:53:47 +0300

    Выполнено третье задание

---
+++ homework_1/task_3.py
@@ -0,0 +1,5 @@
+number = input('Введите число n: ')
+second_number = int(number) * 2
+third_number = int(number) * 3
+result = number + str(second_number) + str(third_number)
+print(f'{number} + {second_number} + {third_number} = {result}')
