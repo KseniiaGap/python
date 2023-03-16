98d77d8de235f002e8fcf9c9759ceb3d772dd71c
Kseniia <kseniyaga@gmail.com>
2023-03-16 13:21:15 +0300

    Выполнено 4-ое задание

---
+++ homework_1/task_4.py
@@ -0,0 +1,11 @@
+revenue = int(input('Введите выручку фирмы: '))
+costs = int(input('Введите издержки фирмы: '))
+if revenue > costs:
+    print(f'Финансовый результат - прибыль. Ее величина: {costs}')
+    return_on_revenue = (revenue - costs) / revenue
+    print(f'Рентабельность выручки = {return_on_revenue}')
+    employee = int(input('Введите численность сотрудников фирмы: '))
+    print(f'Прибыль фирмы в расчете на одного сотрудника = '
+          f'{(revenue - costs) / employee}')
+else:
+    print(f'Финансовый результат - убыток. Его величина: {costs - revenue}')
