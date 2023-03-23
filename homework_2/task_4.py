e295d57a53e69cb380d36be0f01cb72fea1a5ad9
Kseniia <kseniyaga@gmail.com>
2023-03-23 11:24:59 +0300

    Переименовала переменную

--- homework_2/task_4.py
+++ homework_2/task_4.py
@@ -3,8 +3,8 @@
 list_1 = [1, 3, 8, 0, 23, 1]
 list_1.sort(reverse=True)
 print(list_1)
-sum = list_1[0] + list_1[1]
-print(sum)
+summa = list_1[0] + list_1[1]
+print(summa)
 
 
 
@@ -17,5 +17,5 @@ for i in range(len(list_1)):
    if list_1[i] > list_1[i - 1]:
        sec_maximum = maximum
        maximum = list_1[i]
-sum = sec_maximum + maximum
-print(sum)
+summa = sec_maximum + maximum
+print(summa)
