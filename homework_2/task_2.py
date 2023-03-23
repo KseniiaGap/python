d309eb00758b6ad5ca66fedcf837446c1cad7dc2
Kseniia <kseniyaga@gmail.com>
2023-03-23 11:35:59 +0300

    Добавили функцию

--- homework_2/task_2.py
+++ homework_2/task_2.py
@@ -1,7 +1,11 @@
+def func(num_1, num_2):
+    try:
+        res = num_1 / num_2
+    except ZeroDivisionError:
+        res = 'Вы что? Пытаетесь делить на нуль!'
+    print(f'Результат: {res}')
+
+
 number = int(input('Введите первое число: '))
 number_2 = int(input('Введите второе число: '))
-try:
-    res = number / number_2
-except ZeroDivisionError:
-    res = 'Вы что? Пытаетесь делить на нуль!'
-print(f'Результат: {res}')
+func(number, number_2)
